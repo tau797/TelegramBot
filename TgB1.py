076a22612d000973633b21cecd2b7bb2257bacfe
tau797 <165090473+tau797@users.noreply.github.com>
2024-03-27 01:24:40 +0400

    Update TgB1.py

--- TgB1.py
+++ TgB1.py
@@ -4,11 +4,16 @@ import telebot as tb
 # объявляем токен Телеграм-бота
 token = '6886566042:AAGlHIz56IneBdd-G_QKHTmlYZ2fOxBSBOA'
 
+#моё имя Андрей
+hoz = 'Андрей'
+
+#Создание бота 
 bot = tb.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
+    if (hoz in message.text) bot.send_message(message.chat.id, 'Здравствуй, Хозяин!')
 
 # постоянно обращается к серверам ТГ, none stop - не обращать внимание на ошибки!
 bot.polling(none_stop=True)
